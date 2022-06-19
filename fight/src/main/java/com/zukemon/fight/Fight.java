ee6e229bb891dac0cba5698d37a97ee8c8ec17e9
willi.coyote@gmx.de <GitEclipse6>
2022-06-19 20:34:49 +0800

    Initial checkin

---
+++ fight/src/main/java/com/zukemon/fight/Fight.java
@@ -0,0 +1,20 @@
+package com.zukemon.fight;
+
+public class Fight {
+
+    /**
+     * Blastoise
+     * Mew
+     * Wartortle
+     * Mudkip
+     * Pikachu
+     * Psyduck
+     * Krookodile
+     *
+     * @param attackerType
+     * @param defenderType
+     */
+    public int hit(int attackerType, int defenderType) {
+        return 0;
+    }
+}
