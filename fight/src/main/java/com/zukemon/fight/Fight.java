c11fbec1e596af49a0ab16a8e7db56052a1072ea
willi.coyote@gmx.de <GitEclipse6>
2022-06-27 09:29:57 +0800

    Initial checkin

--- fight/src/main/java/com/zukemon/fight/Fight.java
+++ fight/src/main/java/com/zukemon/fight/Fight.java
@@ -3,13 +3,14 @@ package com.zukemon.fight;
 public class Fight {
 
     /**
-     * Blastoise
-     * Mew
-     * Wartortle
-     * Mudkip
-     * Pikachu
-     * Psyduck
-     * Krookodile
+     * Blastoise #009 Water Damage 258
+     * Mew #151 Psychic Damage 150
+     * Wartortle #008 Water Damage 300
+     * Mudkip #258 Water Damage 234
+     * Pikachu #25 Electric Damage 135
+     * Psyduck #54 Water Damage 127
+     * Krookodile #553 Dark No Damage -> It is the team lead, so he can call his team members to arms. Add the
+     * damage of all other Zukemons
      *
      * @param attackerType
      * @param defenderType
